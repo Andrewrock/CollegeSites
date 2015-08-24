bd25ffc9981bf79b8c0a00e71ce57994b1c930e8
Andrew Rock <andrew@deepcovelabs.com>
2015-08-23 21:18:33 -0700

    skelton nearly complete, need to implement routing, and isotope loops

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var appdata = require('/colleges.json');
-var request = require('request');
+//var appdata = require('/colleges.json');
+//var request = require('request');
 
 
 
@@ -19,22 +19,17 @@ router.get('/', function(req, res) {
 //});
 
 
-router.get('/checkout', function(req, res) {
-  //request("https://web-magpie-int-staging.magpiecrm.com/api/cfg/salesoutlet?salesoutletId=3", function (error, response, body) {
-    res.render('checkout', { req:req, res:res,
-      title: 'Checkout'
+router.get('/college', function(req, res) {
+  res.render('college', { req:req, res:res,
+     title: 'Colleges'
     });
   });
-//});
 
-router.get('/plans', function(req, res) {
-  //request("https://web-magpie-int-staging.magpiecrm.com/api/cfg/salesoutlet?salesoutletId=3", function (error, response, body) {
-    res.render('plans', {
-      //req: req, res: res,
-      title: 'Select a plan'
-    });
+router.get('/colleges-course', function(req, res) {
+  res.render('colleges-course', {
+   title: 'Select a plan'
+   });
   });
-//});
 
 
 
